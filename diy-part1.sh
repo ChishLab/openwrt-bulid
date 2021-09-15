2da7ac288f92871461cb9de6c77147738eb99801
Chish <76027184+ChishFoxcat@users.noreply.github.com>
2021-09-15 11:04:58 +0800

    Upload@20210915

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
 
+echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
--- diy-part2.sh
+++ diy-part2.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
 
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
